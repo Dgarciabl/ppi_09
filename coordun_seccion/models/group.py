5b8c84cf7e04ca65602c91e400815f364d1a5e08
Juan Pablo Gómez Reyes <jugomezre@unal.edu.co>
2023-03-23 19:44:07 -0500

    Update group.py

--- coordun_seccion/models/group.py
+++ coordun_seccion/models/group.py
@@ -1,2 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Event(models.group):
+  tema=models.CharField(max_length=250)
+  encargado=models.ForeignKey(User,on_delete=models.CASCADE,related_name="encargado")
+  descripcion=models.TextField(blank=True,null=True)
+  ubicacion=models.CharField(max_length=200)
+  fecha_y_hora=models.DateTimeField(auto_now=True)
+  asistentes=models.ManyToManyField(User,related_name="attendees",through='Attendees')
+  def __str__(self):
+    return self.tema
